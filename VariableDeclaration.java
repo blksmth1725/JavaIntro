9b9ab408256c8ecc1501dae5c072e917e81282ae
Christian Sheen <csheen1725@gmail.com>
2021-11-03 22:49:42 -0400

    All number value data types explored

--- VariableDeclaration.java
+++ VariableDeclaration.java
@@ -3,5 +3,20 @@ public class VariableDeclaration {
   public static void main(String[] args) {
     boolean on = true;
     System.out.println(on);
+
+    short s = 16;
+    System.out.println(s);
+
+    int i = -32;
+    System.out.println(i);
+
+    float f = 696;
+    System.out.println(f);
+
+    long l = 4294967296L;
+    System.out.println(l);
+
+    char c = 'a';
+    System.out.println(c);
   }
 }
