5c1e560bec7afe6b399a3b5d64a32e0be33c9911
Christian Sheen <csheen1725@gmail.com>
2021-11-06 23:37:30 -0400

    Int to a Short upcasting

---
+++ CastingDataTypes.java
@@ -0,0 +1,10 @@
+public class CastingDataTypes {
+  public static void main(String[] args) {
+
+    // int to a double
+    int i = 200;
+    short s = (short) i;
+
+    System.out.println(s);
+  }
+}
