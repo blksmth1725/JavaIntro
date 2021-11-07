83e4d58bce107b9207260844caaf68b4abbf9b99
Christian Sheen <csheen1725@gmail.com>
2021-11-06 23:42:04 -0400

    int -> double (upcasting) && int -> byte (downcasting) which resulted in a negative number

--- CastingDataTypes.java
+++ CastingDataTypes.java
@@ -1,10 +1,18 @@
 public class CastingDataTypes {
   public static void main(String[] args) {
+    // initial value
+    int i = 200;
+
+    // int to a short
+    short s = (short) i;
+    System.out.println(s);
 
     // int to a double
-    int i = 200;
-    short s = (short) i;
+    double d = (double) i;
+    System.out.println(d);
 
-    System.out.println(s);
+    // cast to a byte
+    byte b = (byte) i;
+    System.out.println(b);
   }
 }
