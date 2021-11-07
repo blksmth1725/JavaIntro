ea3035846e513b0fdf2a00e6add5868c58ed2c69
Christian Sheen <csheen1725@gmail.com>
2021-11-07 00:00:30 -0400

    Added To JavaArrays.java new or loop to print elements in an array

--- JavaArrays.java
+++ JavaArrays.java
@@ -12,6 +12,17 @@ public class JavaArrays {
     firstArr[1] = 2;
     firstArr[2] = 3;
 
+    int[] myInts = new int[] { 1, 2, 3, 4 };
+    String languages[] = { "Java", "JavaScript", "SQL" };
+
+    System.out.println(myInts[3]);
+    System.out.println(languages[0]);
+
+    String[] myArr = { "first", "second", "third" };
+    for (int i = 0; i < myArr.length; i++) {
+      System.out.println(myArr[i]);
+    }
+
     System.out.println("The value of firstArr[1] is " + firstArr[1]);
     System.out.println("The length of secondArr is " + secondArr.length);
   }
