5ef1e9c361d5e134bcf3dafe31130f3db41fbf42
Christian Sheen <csheen1725@gmail.com>
2021-11-07 23:17:04 -0500

    Exploring else && else-if statments in Java

--- IfStatments.java
+++ IfStatments.java
@@ -2,10 +2,22 @@ public class IfStatments {
 
   public static void main(String[] args) {
     boolean value = false;
+    boolean firstCondition = false;
+    boolean secondCondition = true;
 
     if (value) {
       System.out.println("We are inside the if-statment");
     }
+
+    if (firstCondition) {
+      System.out.println("We are inside firstCondition if-statment");
+      if (secondCondition) {
+        System.out.println("We are inside secondCondition if-statement");
+      }
+    } else if (secondCondition) {
+      System.out.println(5);
+    }
+
     System.out.println("We are outside the if-statment");
   }
 }
