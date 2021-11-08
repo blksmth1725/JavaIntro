840e849afb7fbd4f3d15210218ab64cfbae8d037
Christian Sheen <csheen1725@gmail.com>
2021-11-07 23:05:36 -0500

    First java instance of an if-statement

---
+++ IfStatments.java
@@ -0,0 +1,11 @@
+public class IfStatments {
+
+  public static void main(String[] args) {
+    boolean value = false;
+
+    if (value) {
+      System.out.println("We are inside the if-statment");
+    }
+    System.out.println("We are outside the if-statment");
+  }
+}
