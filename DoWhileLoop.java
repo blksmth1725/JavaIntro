aa3dd5533fc3fddc69d74d2a96c4e3bbcd9f8f88
Christian Sheen <csheen1725@gmail.com>
2021-11-08 22:17:17 -0500

    Do-While Loop in Java

---
+++ DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+
+  public static void main(String[] args) {
+    int index = 0;
+
+    do {
+      index++;
+      System.out.println("Hello, again");
+    } while (index < 10);
+  }
+}
