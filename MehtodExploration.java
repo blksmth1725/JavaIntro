0fc7cc1b5c09019440ae092c4283efa508e7c288
Christian Sheen <csheen1725@gmail.com>
2021-11-09 22:32:09 -0500

    Create two methods 1) Method that returns the value of Pi && 2) Method that returns an array of alphabetical characters

--- MehtodExploration.java
+++ MehtodExploration.java
@@ -4,11 +4,26 @@ public class MehtodExploration {
     MehtodExploration method = new MehtodExploration();
 
     int number = method.randomNumber();
+    double pi = method.getPi();
+    char[] arr = method.getAlphabetArray();
+
     System.out.println(number);
+    System.out.println(pi);
+    System.out.println(arr);
   }
 
   public int randomNumber() {
     int result = (int) (Math.random() * 50) + 1;
     return result;
+  }
+
+  public double getPi() {
+    double pi = (double) (Math.PI);
+    return pi;
+  }
+
+  public char[] getAlphabetArray() {
+    char[] letter = { 'a', 'b', 'c', 'd' };
+    return letter;
   }
 }
